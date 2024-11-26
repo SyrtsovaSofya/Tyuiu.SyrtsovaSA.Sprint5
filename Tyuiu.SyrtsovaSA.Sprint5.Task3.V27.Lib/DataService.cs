e26a0462e3374b48ff2fcfecdf3231ec017fc741
Linn <worldof.s@yandex.ru>
2024-11-26 16:40:48 +0500

    Task3

---
+++ Tyuiu.SyrtsovaSA.Sprint5.Task3.V27.Lib/DataService.cs
@@ -0,0 +1,18 @@
+using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.SyrtsovaSA.Sprint5.Task3.V27.Lib;
+
+public class DataService : ISprint5Task3V27
+{
+    public string SaveToFileTextData(int x)
+    {
+        string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+        double y = Math.Round(Math.Pow(x - 1, 3 * x + 1), 3);
+        using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), encoding: Encoding.UTF8))
+        {
+            writer.Write(BitConverter.GetBytes(y));
+        }
+        return path;
+    }
+}

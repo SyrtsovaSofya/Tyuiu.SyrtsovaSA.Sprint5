0f03e70d2ac1584509dffbd3d37b300e36ea97c2
Linn <worldof.s@yandex.ru>
2024-11-26 19:36:36 +0500

    Task5

--- Tyuiu.SyrtsovaSA.Sprint5.Task5.V30.Lib/DataService.cs
+++ Tyuiu.SyrtsovaSA.Sprint5.Task5.V30.Lib/DataService.cs
@@ -1,6 +1,6 @@
 using System;
-using tyuiu.cources.programming.interfaces.Sprint5; 
- 
+using tyuiu.cources.programming.interfaces.Sprint5;
+
 namespace Tyuiu.SyrtsovaSA.Sprint5.Task5.V30.Lib;
 
 public class DataService : ISprint5Task5V30
@@ -18,11 +18,18 @@ public class DataService : ISprint5Task5V30
                 foreach (string strNum in strNums)
                 {
                     double num = (Convert.ToDouble(strNum));
-                    if (num > max && (int)num % 2 != 0)
-                        max = num;
+                    if ((int)num == num && num > max)
+                    {
+                        bool isPrime = true;
+                        for (int i = 2; i <= num / 2; i++)
+                            if (num % i == 0)
+                                isPrime = false;
+                        if (isPrime || num == 2)
+                            max = num;
+                    }
                 }
             }
+            return max;
         }
-        return Math.Round(max, 3);
     }
-} 
+}

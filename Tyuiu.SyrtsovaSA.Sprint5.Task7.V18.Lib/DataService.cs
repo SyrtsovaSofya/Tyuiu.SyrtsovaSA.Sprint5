73fb5416d383d18874af2f213a91da7f20a0e3f9
Linn <worldof.s@yandex.ru>
2024-11-27 18:49:44 +0500

    Task7

---
+++ Tyuiu.SyrtsovaSA.Sprint5.Task7.V18.Lib/DataService.cs
@@ -0,0 +1,18 @@
+using tyuiu.cources.programming.interfaces.Sprint5; 
+ 
+namespace Tyuiu.SyrtsovaSA.Sprint5.Task7.V18.Lib;
+
+public class DataService : ISprint5Task7V18
+{
+    public string LoadDataAndSave(string path)
+    {
+        string pathSaveFile = Path.Combine(Path.GetTempPath(), "OutPutFileTask7.txt");
+        FileInfo fileInfo = new FileInfo(pathSaveFile);
+        if (fileInfo.Exists )
+            File.Delete(pathSaveFile);
+        string str = File.ReadAllText(path);
+        str = str.Replace("н", "нн");
+        File.AppendAllText(pathSaveFile, str);
+        return pathSaveFile;
+    }
+} 
---
+++ Tyuiu.SyrtsovaSA.Sprint5.Task7.V18.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.SyrtsovaSA.Sprint5.Task7.V18.Lib; 
+ 
+namespace Tyuiu.SyrtsovaSA.Sprint5.Task7.V18.Test; 
+ 
+[TestClass] 
+public class DataServiceTest 
+{ 
+   [TestMethod]
+    public void CheckIfFileExists()
+    {
+        string path = @"C:\DataSprint5\InPutDataFileTask7V18.txt";
+        FileInfo fileInfo = new FileInfo(path);
+        bool fileExists = fileInfo.Exists;
+        Assert.AreEqual(true, fileExists);
+    }
+} 

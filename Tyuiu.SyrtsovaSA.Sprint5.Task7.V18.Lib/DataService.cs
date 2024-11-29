bebc63979f5be769a30669a1d9d44b2ce485b724
Linn <worldof.s@yandex.ru>
2024-11-29 17:41:01 +0500

    Task7

--- Tyuiu.SyrtsovaSA.Sprint5.Task7.V18.Lib/DataService.cs
+++ Tyuiu.SyrtsovaSA.Sprint5.Task7.V18.Lib/DataService.cs
@@ -1,15 +1,16 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint5;
 
-namespace Tyuiu.SyrtsovaSA.Sprint5.Task7.V18.Lib;
-
-public class DataService : ISprint5Task7V18
+namespace Tyuiu.SyrtsovaSA.Sprint5.Task7.V18.Lib
 {
-    public string LoadDataAndSave(string path)
+    public class DataService : ISprint5Task7V18
     {
-        string str = File.ReadAllText(path);
-        str = str.Replace("н", "нн");
-        string pathSaveFile = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V18.txt");
-        File.WriteAllText(pathSaveFile, str);
-        return pathSaveFile; 
+        public string LoadDataAndSave(string path)
+        {
+            string str = File.ReadAllText(path);
+            str = str.Replace("н", "нн");
+            string pathSaveFile = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V18.txt");
+            File.WriteAllText(pathSaveFile, str);
+            return pathSaveFile;
+        }
     }
 }
--- Tyuiu.SyrtsovaSA.Sprint5.Task7.V18.Test/DataServiceTest.cs
+++ Tyuiu.SyrtsovaSA.Sprint5.Task7.V18.Test/DataServiceTest.cs
@@ -1,14 +1,15 @@
-namespace Tyuiu.SyrtsovaSA.Sprint5.Task7.V18.Test;
-
-[TestClass]
-public class DataServiceTest
+namespace Tyuiu.SyrtsovaSA.Sprint5.Task7.V18.Test
 {
-    [TestMethod]
-    public void CheckIfFileExists()
+    [TestClass]
+    public class DataServiceTest
     {
-        string path = @"C:\DataSprint5\InPutDataFileTask7V18.txt";
-        FileInfo fileInfo = new FileInfo(path);
-        bool fileExists = fileInfo.Exists;
-        Assert.AreEqual(true, fileExists);
+        [TestMethod]
+        public void CheckIfFileExists()
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V18.txt");
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
+        }
     }
 }
